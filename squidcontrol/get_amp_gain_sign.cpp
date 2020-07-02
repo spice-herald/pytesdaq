5ab14ac84f108ae5159c3c38eeb2ff990b30751b
Vetri Velan Asus Laptop <velanvetri5@gmail.com>
2020-07-02 16:07:13 -0700

    Moved setup code at beginning of set/get functions to a single function

--- squidcontrol/get_amp_gain_sign.cpp
+++ squidcontrol/get_amp_gain_sign.cpp
@@ -13,7 +13,7 @@ int main(int argc, char** argv) {
     // Check arguments
     if (argc != 3 && argc != 5) {
         cout << "ERROR: run the program as follows" << endl;
-        cout << "\t.\\get_dummy.exe channel[1,2,3] active[0,1] [baud = 57600] [timeout = 100]" << endl;
+        cout << "\t.\\get_amp_gain_sign.exe channel[1,2,3] active[0,1] [baud = 57600] [timeout = 100]" << endl;
         cout << "\tNote: either set both baud and timeout or neither." << endl;
         cout << "\tNote: active indicates whether to make the channel the active channel." << endl;
         cout << flush;
--- squidcontrol/squid_control_helpers.cpp
+++ squidcontrol/squid_control_helpers.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <time.h>
-#include <thread>
-#include <chrono>
 #include <string>
+#include <limits>
 #include "magsv.h"
 
 using namespace std;
@@ -13,8 +11,61 @@ const double PHIB_MIN = -125., PHIB_MAX = 125.;
 const double IAUX_LOW_MIN = -125., IAUX_LOW_MAX = 125.;
 const double IAUX_HIGH_MIN = -500., IAUX_HIGH_MAX = 500.;
 
+
+
 void errorout(unsigned short error) {
     if (error != 0)
         cout << "\nCommunication Error" << endl;
 }
 
+
+
+// Check user's command-line arguments for general validity
+// Check the number of arguments given, and if appropriate, open USB connection and set the active channel.
+// Arguments: argc and argv from the command-line arguments; the number of extra arguments in addition to
+// the call, channel, whether to set active, baud, and timeout (i.e. argc minus 5); the function call
+// in the form *.exe
+// Returns: an unsigned short array with three elements: channel, active, and error
+
+unsigned short* validate_args(int argc, char** argv, int extra_args, string exe_name) {
+
+    unsigned short containers[3] = {(unsigned short) USHRT_MAX, (unsigned short) USHRT_MAX, (unsigned short) USHRT_MAX};
+
+    // Check number of arguments
+    if (argc != (3 + extra_args) && argc != (5 + extra_args)) {
+        cout << "ERROR: run the program as follows" << endl;
+        printf("\t.\\%s channel[1,2,3] active[0,1] [baud = 57600] [timeout = 100]\n", exe_name);
+        cout << "\tNote: either set both baud and timeout or neither." << endl;
+        cout << "\tNote: active indicates whether to make the channel the active channel." << endl;
+        cout << flush;
+        return containers;
+    }
+
+    // Set connection info
+    unsigned long baud = 57600, timeout = 100; 
+    if (argc == (5 + extra_args)) {
+        baud = stoul(argv[3 + extra_args]);
+        timeout = stoul(argv[4 + extra_args]);
+    }
+
+    // Set containers: channel, whether to set active, error
+    unsigned short channel = (unsigned short)(stoul(argv[1]));
+    unsigned short active = (unsigned short)(stoul(argv[2]));
+    unsigned short error = 0;
+
+    // Connect to electronics
+    MA_initUSB(&error, baud, timeout);
+    errorout(error);
+
+    // Set active channel if desired
+    if (active == 1) {
+        MA_SetActiveChannel(channel, &error);
+        errorout(error);
+    }
+
+    // Return containers
+    containers[0] = channel;
+    containers[1] = active;
+    containers[2] = error;
+    return containers;
+}
