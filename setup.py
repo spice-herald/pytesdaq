edb79e51b1492f3b96519ecdb4d651a6d86bf627
Samuel Watkins <35979561+slwatkins@users.noreply.github.com>
2022-01-17 08:33:14 -0800

    add long description for putting README on PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+      long_description = f.read()
+
+
 setup(name='pytesdaq',
       version='0.1.1',
       description='DAQ and Intruments control for TES development',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Bruno Serfass',
       author_email='serfass@berkeley.edu',
       zip_safe = False,
