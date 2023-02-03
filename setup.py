c5a6a690dcee3fd959e57d2fe53ff329ae0d5fe4
Bruno Serfass <serfass@oak.berkeley.edu>
2023-02-02 16:14:49 -0800

    speed up read_many_events

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 
 setup(name='pytesdaq',
-      version='0.2.9',
+      version='0.3.0',
       description='DAQ and Intruments control for TES development',
       long_description=long_description,
       long_description_content_type='text/markdown',
